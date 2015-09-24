b7dbb9afa164f4a429103d90275825e26d441715
Edgar Teroyan <edgar_teroyan@yahoo.com>
2015-09-24 17:55:16 +0400

    Worked on merging films for cp and store

--- app/Http/Controllers/TitlesController.php
+++ app/Http/Controllers/TitlesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Http\Requests;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Debug\Dumper;
 use DB;
 
@@ -30,18 +31,33 @@ class TitlesController extends Controller
         $company_info = $account_info->company;
         //(new Dumper)->dump($company_info->toArray());
 
-        $films = $company_info->films()->where('cc_films.deleted', '0')->get();
-        //(new Dumper)->dump($films->toArray());
-
-        $film_cp = $films->first()->companies()->where('fk_films_owners.type', '0')->get();
-        //(new Dumper)->dump($film_cp->toArray());
-
+        $company_films = $company_info->films()->where('cc_films.deleted', '0')->get();
 
 
         $store_info = $account_info->store;
         //(new Dumper)->dump($store_info->toArray());
 
         $store_films = $store_info->contracts()->with('films', 'stores')->get();
+
+        foreach($company_films as $key=>$company_film){
+            $company_film_stores = $company_films->first()->baseContract()->with('stores')->get();
+
+            $company_film->stores = $company_film_stores->first()->stores;
+            $company_film->companies = $company_film->companies()->where('fk_films_owners.type', '0')->get();
+        }
+
+
+        foreach($store_films as $key=>$store_film){
+            $store_film->films->stores = $store_film->stores;
+            $store_film->films->companies = $store_film->films->companies()->where('fk_films_owners.type', '0')->get();
+            $store_films[$key] = $store_film->films;
+            //unset($store_films[$key]->stores);
+        }
+
+        $company_films = $company_films->keyBy('id');
+        $store_films = $store_films->keyBy('id');
+        $films = $company_films->merge($store_films);
+        dd($films);
         //$store_films = $store_films->toArray();
         //(new Dumper)->dump($store_films[0]['stores']);
         //(new Dumper)->dump($store_films[0]['films']);
